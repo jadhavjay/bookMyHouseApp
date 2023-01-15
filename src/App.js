010074d76ca8b9db88f36bc1ba73e2711451d647
jadhavjay <jadhavjai02@gmail.com>
2023-01-15 14:36:15 +0530

    added login form and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/Login'
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
